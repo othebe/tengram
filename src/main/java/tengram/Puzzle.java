10cb9bac452fb7c86800ae0378812c75616c9fe6
othebe <othebe@gmail.com>
2018-04-27 11:13:54 -0700

    Updated question+solution

--- src/main/java/tengram/Puzzle.java
+++ src/main/java/tengram/Puzzle.java
@@ -6,16 +6,16 @@ import tengram.arranger.BoardArranger;
 
 public class Puzzle {
     private static Piece[] PIECES = {
-            new Piece("A", 2, 9),
-            new Piece("B", 2, 1),
-            new Piece("C", 2, 5),
+            new Piece("A", 4, 3),
+            new Piece("B", 2, 5),
+            new Piece("C", 1, 8),
             new Piece("D", 7, 2),
-            new Piece("E", 2, 3),
-            new Piece("F", 5, 5),
-            new Piece("G", 4, 3),
-            new Piece("H", 1, 3),
-            new Piece("I", 1, 2),
-            new Piece("J", 1, 8)
+            new Piece("E", 5, 5),
+            new Piece("F", 9, 2),
+            new Piece("G", 1, 3),
+            new Piece("H", 1, 2),
+            new Piece("I", 2, 1),
+            new Piece("J", 2, 3)
     };
 
     public static void main(String[] args) {
--- src/main/java/tengram/arranger/BoardArranger.java
+++ src/main/java/tengram/arranger/BoardArranger.java
@@ -1,10 +1,108 @@
 package tengram.arranger;
 
 import tengram.core.Board;
+import tengram.core.Orientation;
 import tengram.core.Piece;
 
 public class BoardArranger {
     public static boolean arrange(Board board, Piece[] pieces) {
-        return true;
+        return arrange(board, pieces, 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static boolean arrange(Board board, Piece[] pieces, int pieceNdx) {
+        if (pieceNdx >= pieces.length) {
+            return true;
+        }
+
+        Piece piece = pieces[pieceNdx];
+        for (int x = 0; x < board.getWidth(); x++) {
+            for (int y = 0; y < board.getHeight(); y++) {
+                if (board.canPlacePiece(piece, x, y, Orientation.NORMAL)) {
+                    board.placePiece(piece, x, y, Orientation.NORMAL);
+                } else if (board.canPlacePiece(piece, x, y, Orientation.CLOCKWISE)) {
+                    board.placePiece(piece, x, y, Orientation.CLOCKWISE);
+                } else {
+                    continue;
+                }
+
+                if (arrange(board, pieces, pieceNdx + 1)) {
+                    return true;
+                } else {
+                    board.removePieceFromBoard(piece);
+                }
+            }
+        }
+
+        return false;
     }
 }
